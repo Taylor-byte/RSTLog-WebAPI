f0f55e0804336e72bd9bbef9cc1050722fc4dcd6
Jack Taylor <jack.taylor10@outlook.com>
2021-03-09 20:48:01 +0000

    Repositories and Dependency Injection
    Repository and unit of work pattern for separation of concerns and reduce repetition.

---
+++ WebAPI/IRepository/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Customer> Customer { get; }
+
+        IGenericRepository<Days> Days { get; }
+
+        IGenericRepository<Hours> Hours { get; }
+
+        IGenericRepository<Employee> Employee { get; }
+
+        Task Save();
+        
+
+    }
+}
---
+++ WebAPI/Repository/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.IRepository;
+using WebAPI.Models;
+
+namespace WebAPI.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        private IGenericRepository<Customer> _customer;
+
+        private IGenericRepository<Days> _days;
+
+        private IGenericRepository<Hours> _hours;
+
+        private IGenericRepository<Employee> _employee; 
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+
+            
+
+        }
+
+        public IGenericRepository<Customer> Customer => _customer ??= new GenericRepository<Customer>(_context);
+
+        public IGenericRepository<Days> Days => _days ??= new GenericRepository<Days>(_context);
+
+        public IGenericRepository<Hours> Hours => _hours ??= new GenericRepository<Hours>(_context);
+
+        public IGenericRepository<Employee> Employee => _employee ??= new GenericRepository<Employee>(_context);
+
+        public void Dispose()
+        {
+            //garbage collect any unused memory
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
